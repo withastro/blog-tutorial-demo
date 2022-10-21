6d2c8c571cf0939a280696fe942809f07ed88293
Dan Jutan <danjutan@gmail.com>
2022-10-21 16:31:05 -0400

    end-of-6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact()]
+});
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,18 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+  
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div> 
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
