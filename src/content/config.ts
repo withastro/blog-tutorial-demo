64e981c7b13f4626143b6cab4151302610a94995
Dan Jutan <danjutan@gmail.com>
2023-01-29 11:27:45 -0500

    tags aren't optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,5 @@
-// src/content/config.ts
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
-// 2. Define your collection(s)
+
 const postsCollection = defineCollection({
     schema: z.object({
       layout: z.optional(z.any()),
@@ -13,11 +11,10 @@ const postsCollection = defineCollection({
         url: z.string(),
         alt: z.string()
       }),
-      tags: z.array(z.string()).optional()
+      tags: z.array(z.string())
     })
  });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+
 export const collections = {
   posts: postsCollection,
 };
