33226dbfe260eceeda1fffcfc5f82d28a0a313d1
Dan Jutan <danjutan@gmail.com>
2023-01-29 11:25:52 -0500

    remove comment

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,8 +11,6 @@ export async function get(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/posts/${post.slug}/`,
     })),
   });
