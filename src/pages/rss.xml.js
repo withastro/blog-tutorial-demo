e91cb38d165ea84b4d00a287a4c12d7314af5873
Dan Jutan <danjutan@gmail.com>
2023-01-31 16:31:12 -0500

    remove unused import in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+export async function get() {
   const posts = await getCollection('posts');
   return rss({
     title: 'Astro Learner | Blog',
