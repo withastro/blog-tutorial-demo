5e66dc62628948babf542c1f369207d9cb55ba66
Chris Swithinbank <swithinbank@gmail.com>
2023-03-23 18:28:17 +0100

    Implement fix from https://github.com/withastro/docs/pull/2908

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -5,7 +5,7 @@ export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
   
-  const [greeting, setGreeting] = useState(randomMessage());
+  const [greeting, setGreeting] = useState(messages[0]);
 
   return (
     <div> 
