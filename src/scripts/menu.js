6df7dc4b8fd25e303f4974cca119adcdcf51da13
Sarah Rainsberger <sarah@rainsberger.ca>
2023-10-10 01:11:48 +0000

    basic view transitions implementation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+document.addEventListener('astro:page-load', () => {
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+});
 });
