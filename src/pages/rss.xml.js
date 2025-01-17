47ecf1a992ddda4fb37d07611dbe5662e154dee2
Armand Philippot <git@armand.philippot.eu>
2025-01-18 00:46:03 +0100

    update RSS feed (post.id, context)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,17 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-export async function GET() {
+export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `/posts/${post.id}/`,
     })),
     customData: `<language>en-us</language>`,
   });
