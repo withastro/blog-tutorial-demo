9b34d1fd17d2829ce39154a5658a49e69f087c07
Armand Philippot <git@armand.philippot.eu>
2025-01-18 00:41:38 +0100

    add ts-check in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  site: "https://example.com",
+  integrations: [preact()],
 });
