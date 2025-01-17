80a8cd89046d7829a42cb55a3aed616f4a41da13
Armand Philippot <git@armand.philippot.eu>
2025-01-18 00:42:01 +0100

    convert Greeting component to JSX to avoid TS error and match the docs

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,14 +1,13 @@
-import { h } from 'preact';
 import { useState } from 'preact/hooks';
 
 export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-  
+
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div> 
+    <div>
       <h3>{greeting}! Thank you for visiting!</h3>
       <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
