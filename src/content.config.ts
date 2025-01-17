1f95c1854afe031c5c66d2e7da3143853d5e2abd
Armand Philippot <git@armand.philippot.eu>
2025-01-18 00:45:09 +0100

    remove unnecessary escape character in content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,18 +4,18 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**\/[^_]*.md', base: "./src/blog" }),
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
-    })
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = { blog };
