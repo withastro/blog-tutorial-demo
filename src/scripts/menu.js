0859a3ea85f35325c0a292f9b3131824ef95ee38
Armand Philippot <git@armand.philippot.eu>
2025-10-11 15:00:29 +0200

    fix(complete): update deps, clean up & fix mismatches with docs (#45)
    * fix(complete): update deps, clean up & fix mismatches with docs
    * fix: keep Typescript code in `menu.js`

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('.menu')
 
-menu.addEventListener('click', () => {
-  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
-  menu.setAttribute('aria-expanded', !isExpanded);
+menu?.addEventListener("click", () => {
+  const isExpanded = menu.getAttribute("aria-expanded") === "true";
+  menu.setAttribute("aria-expanded", `${!isExpanded}`);
 });
